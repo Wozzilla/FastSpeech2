cd82f3ea3f30567eacff75d96752b3cd3cfc51b9
YukiShionji <80265989+YukiShionji@users.noreply.github.com>
2024-01-12 09:04:05 +0800

    Update prepare_align.py.
    Delete some useless code.

--- prepare_align.py
+++ prepare_align.py
@@ -5,14 +5,9 @@ import yaml
 from preprocessor import ljspeech, aishell3, libritts
 
 
-def main(config):
-    if "LJSpeech" in config["dataset"]:
-        ljspeech.prepare_align(config)
-    if "AISHELL3" in config["dataset"]:
-        aishell3.prepare_align(config)
-    if "LibriTTS" in config["dataset"]:
-        libritts.prepare_align(config)
-
+def main(config): 
+    aishell3.prepare_align(config)
+   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
